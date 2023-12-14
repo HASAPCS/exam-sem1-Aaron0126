f18a327ad08fffa539d232faea958909eebfc1cf
Aaron0126 <aaron.lin@has.hc.edu.tw>
2023-12-14 11:12:59 +0800

    second one

--- src/main/java/com/example/StepTracker.java
+++ src/main/java/com/example/StepTracker.java
@@ -1,3 +1,37 @@
 package com.example;
 
 //write your class implementation here
+public class StepTracker{
+    private int minSteps;
+    private int activeDays;
+    private int totalSteps;
+    private int noDays;
+
+    public StepTracker (int minSteps){
+        this.minSteps=minSteps;
+        this.activeDays=0;
+        this.totalSteps=0;
+        this.noDays=0;
+    }    
+    public int getMinSteps(){
+        return this.minSteps;
+    }
+    public int activeDays(){
+        return this.activeDays;
+    }
+    public double averageSteps(){
+        if (noDays>0){
+            return (double) totalSteps/noDays;
+        }
+        return 0;
+    }
+    public void addDailySteps(int todaySteps){
+        if (todaySteps>minSteps){
+            activeDays++;
+        }
+        totalSteps+=todaySteps;
+        noDays++;
+
+    }
+    
+}   
